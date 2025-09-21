b0e7523cc240fd0972cce483f384c18e593653d1
nguyenhuuanh21 <nguyenhuuanh.user@gmail.com>
2025-09-21 11:22:20 +0700

    commit cors configuration

--- backend/src/main/java/vn/backend/backend/config/AppConfig.java
+++ backend/src/main/java/vn/backend/backend/config/AppConfig.java
@@ -1,13 +1,18 @@
 package vn.backend.backend.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 @Configuration
@@ -33,6 +38,19 @@ public class AppConfig {
                             "/swagger-ui.html",
                             "/swagger-ui/**",
                             "/swagger-ui/**/swagger-initializer.js");
+        }
+        @Bean
+        public FilterRegistrationBean<CorsFilter> corsFilter() {
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            CorsConfiguration config=new CorsConfiguration();
+            config.setAllowCredentials(true);
+            config.addAllowedOrigin("*");
+            config.addAllowedHeader("*");
+            config.addAllowedMethod("*");
+            source.registerCorsConfiguration("/**", config);
+            FilterRegistrationBean bean=new FilterRegistrationBean<>(new CorsFilter(source));
+            bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+            return bean;
         }
         @Bean
         public PasswordEncoder passwordEncoder(){
