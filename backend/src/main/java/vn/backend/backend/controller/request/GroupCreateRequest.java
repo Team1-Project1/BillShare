51263b29f2f0237e31430eaa6e28c509f6849192
nguyenhuuanh21 <nguyenhuuanh.user@gmail.com>
2025-11-15 16:41:16 +0700

    bo sung request tao nhom

--- backend/src/main/java/vn/backend/backend/controller/request/GroupCreateRequest.java
+++ backend/src/main/java/vn/backend/backend/controller/request/GroupCreateRequest.java
@@ -2,8 +2,13 @@ package vn.backend.backend.controller.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 public class GroupCreateRequest {
     private String groupName;
     private String description;
